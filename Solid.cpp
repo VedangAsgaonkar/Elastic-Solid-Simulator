45d2dc754894f283461a34107e101e8b3877fef4
VedangAsgaonkar <81141571+VedangAsgaonkar@users.noreply.github.com>
2021-05-14 16:20:48 +0530

    Update Solid.cpp

--- Solid.cpp
+++ Solid.cpp
@@ -26,6 +26,29 @@ class Shape : public Fl_Widget{
 		virtual void refresh(double dx, double dy)=0;
 };
 
+class Rectangle: public Shape{
+	private:
+	int x, y , w , h ;
+	public:
+	Rectangle(int x , int y, int w, int h) : Shape(x,y,w,h){
+		this->x = x ;
+		this->y = y ;
+		this->w = w ;
+		this->h = h ;
+		
+	}
+	virtual void draw(){
+		fl_draw_box(FL_FLAT_BOX , x , y , w , h ,FL_RED );
+	}
+	virtual void wipe(){
+		;
+	}
+	virtual void refresh(double x, double y){
+		;
+	}
+
+};
+
 class Circle : public Shape{
 	private:
 	
@@ -85,11 +108,16 @@ class Wrapper{
 		link* d_nhbrs ;
 		double spring_const  ;	
 		double natural_length ;	
+		double min_length ;
+		double max_length ;
 		double d_spring_const  ; 
 		double d_natural_length ;
+		double d_min_length ;
+		double d_max_length ;		
 		double eta ;
+		
 	public:
-		Wrapper( int n_c , Circle* circles[], int n_l , link link_list[] , int d_n_l , link d_link_list[] , double sc  , double nat_len ,double d_sc, double d_nat_len , double eta1){
+		Wrapper( int n_c , Circle* circles[], int n_l , link link_list[] , int d_n_l , link d_link_list[] , double sc  , double nat_len , double min_len , double max_len ,double d_sc, double d_nat_len, double d_min_len, double d_max_len , double eta1){
 			num_circles = n_c ;
 			for(int i = 0 ; i < num_circles ; i++){
 				c[i] = circles[i] ;
@@ -111,13 +139,21 @@ class Wrapper{
 			eta = eta1;	
 			d_spring_const = d_sc;
 			d_natural_length = d_nat_len ;		
+			min_length = min_len ;
+			max_length = max_len ;
+			d_min_length = d_min_len ;
+			d_max_length = d_max_len ;
 		}
 		
 		void refreshAll(){
 			
 			if(trigger){
 				
-				c[45]->refresh(45,45);
+				for(int j=0 ; j <10 ; j++){
+					for(int i=0 ; i <10; i++){
+						c[10*j + i] -> v_x = 12 ;
+					}					
+				}
 				trigger = false ;
 			}
 			
@@ -130,7 +166,21 @@ class Wrapper{
 				nhbrs[i].first->a_y += spring_const*y_proj*ext;
 				nhbrs[i].second->a_x += -spring_const*x_proj*ext;
 				nhbrs[i].second->a_y += -spring_const*y_proj*ext;
-			}
+				double rel_rad_vel = (nhbrs[i].second->v_x - nhbrs[i].first->v_x)*x_proj + (nhbrs[i].second->v_y - nhbrs[i].first->v_y)*y_proj ;
+				nhbrs[i].first->a_x += eta*rel_rad_vel*x_proj ;
+				nhbrs[i].first->a_y += eta*rel_rad_vel*y_proj ;
+				nhbrs[i].second->a_x += -eta*rel_rad_vel*x_proj ;
+				nhbrs[i].second->a_y += -eta*rel_rad_vel*y_proj ;
+				
+				double sep = ext +  natural_length;
+				if(sep<=min_length || sep>=max_length ){
+					nhbrs[i].first->v_x = nhbrs[i].first->v_x*y_proj*y_proj + nhbrs[i].second->v_x*x_proj*x_proj + (nhbrs[i].first->v_y - nhbrs[i].second->v_y)*x_proj*y_proj ;
+					nhbrs[i].first->v_y = nhbrs[i].first->v_y*x_proj*x_proj + nhbrs[i].second->v_y*y_proj*y_proj + (nhbrs[i].first->v_x - nhbrs[i].second->v_x)*x_proj*y_proj ;
+					nhbrs[i].second->v_x = nhbrs[i].second->v_x*y_proj*y_proj + nhbrs[i].first->v_x*x_proj*x_proj + (nhbrs[i].second->v_y - nhbrs[i].first->v_y)*x_proj*y_proj ;
+					nhbrs[i].second->v_y = nhbrs[i].second->v_y*x_proj*x_proj + nhbrs[i].first->v_y*y_proj*y_proj + (nhbrs[i].second->v_x - nhbrs[i].first->v_x)*x_proj*y_proj ;
+				}
+			}	
+			
 			
 			for(int i=0 ; i <d_num_links ; i++ ){
 				
@@ -141,10 +191,26 @@ class Wrapper{
 				d_nhbrs[i].first->a_y += d_spring_const*y_proj*ext;
 				d_nhbrs[i].second->a_x += -d_spring_const*x_proj*ext;
 				d_nhbrs[i].second->a_y += -d_spring_const*y_proj*ext;
+				double rel_rad_vel = (d_nhbrs[i].second->v_x - d_nhbrs[i].first->v_x)*x_proj + (d_nhbrs[i].second->v_y - d_nhbrs[i].first->v_y)*y_proj ;
+				d_nhbrs[i].first->a_x += eta*rel_rad_vel*x_proj ;
+				d_nhbrs[i].first->a_y += eta*rel_rad_vel*y_proj ;
+				d_nhbrs[i].second->a_x += -eta*rel_rad_vel*x_proj ;
+				d_nhbrs[i].second->a_y += -eta*rel_rad_vel*y_proj ;
+				
+				double sep = ext +  d_natural_length;
+				if(sep<=d_min_length || sep>=d_max_length ){
+					d_nhbrs[i].first->v_x = d_nhbrs[i].first->v_x*y_proj*y_proj + d_nhbrs[i].second->v_x*x_proj*x_proj + (d_nhbrs[i].first->v_y - d_nhbrs[i].second->v_y)*x_proj*y_proj ;
+					d_nhbrs[i].first->v_y = d_nhbrs[i].first->v_y*x_proj*x_proj + d_nhbrs[i].second->v_y*y_proj*y_proj + (d_nhbrs[i].first->v_x - d_nhbrs[i].second->v_x)*x_proj*y_proj ;
+					d_nhbrs[i].second->v_x = d_nhbrs[i].second->v_x*y_proj*y_proj + d_nhbrs[i].first->v_x*x_proj*x_proj + (d_nhbrs[i].second->v_y - d_nhbrs[i].first->v_y)*x_proj*y_proj ;
+					d_nhbrs[i].second->v_y = d_nhbrs[i].second->v_y*x_proj*x_proj + d_nhbrs[i].first->v_y*y_proj*y_proj + (d_nhbrs[i].second->v_x - d_nhbrs[i].first->v_x)*x_proj*y_proj ;
+				}
 				
 			}	
 			
 			for(int i=0 ; i<num_circles ; i++){
+				if(c[i]->x > 1380 ){
+					c[i]->v_x = - c[i]->v_x ;	
+				}
 				c[i]->refresh() ;
 			}
 		
@@ -181,14 +247,15 @@ class MyTimer{
 };
 
 int main(){
-	Fl_Window* window = new Fl_Window(1000,1000,"H2O") ;
+	Fl_Window* window = new Fl_Window(1500,1000,"Solid") ;
+	Rectangle* wall = new Rectangle(1400,0,100,1000);
 	Circle* c[100] ;
 	link link_list[5050] ;
 	link d_link_list[5050] ;
 	int top = 0 ;
 	int d_top = 0;
 	for(int i=0 ; i < 100; i ++){
-		c[i] = new Circle(50+100*(i%10),50+100*(i/10),10);
+		c[i] = new Circle(800+50*(i%10),300+50*(i/10),6);
 	}
 	for(int i=0 ; i< 10 ; i++ ){
 		for(int j=0 ; j<10 ; j++){
@@ -215,7 +282,7 @@ int main(){
 		}
 	}
 	
-	Wrapper* wrapper = new Wrapper(100,c,top,link_list,d_top,d_link_list,0.1,100,0.07,100*sqrt(2),0.01);
+	Wrapper* wrapper = new Wrapper(100,c,top,link_list,d_top,d_link_list,0.4,50, 10, 75 , 0.28,50*sqrt(2), 10 , 100 , 0.02);
 	trigger = true;
 	MyTimer* t = new MyTimer;
 	t->start(wrapper);
